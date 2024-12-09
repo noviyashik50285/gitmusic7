f3dc40a0a43373353bd3cfc6c603730e581f32d2
noviyashik50285 <mynewdisk50285@gmail.com>
2024-12-10 00:08:08 +0300

    sec7 commit

--- vklvikl.js
+++ vklvikl.js
@@ -1,19 +1,12 @@
-//document.getElementById("knopa").style.display="none";
-var a;
-function show_hide()
-{
-if(a==1) {
-    document.getElementById("victor").style.display="inline";
-    return a=0;
-}
-else{
+const ubrfon = document.getElementById('ubratfon');
+ubrfon.addEventListener('click', function() {
     document.getElementById("containerop1").style.display="none";
     document.getElementById("containerop2").style.display="none";
     document.getElementById("containerop3").style.display="none";
     document.getElementById("containerop4").style.display="none";
     document.getElementById("containerop5").style.display="none";
-    return a=1;
-}
-}
+});
+
+
 
 
