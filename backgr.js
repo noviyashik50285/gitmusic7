44eebdea0797e9a94ad9678b2a1b464aa45ae188
noviyashik50285 <mynewdisk50285@gmail.com>
2024-12-22 22:40:09 +0300

    sec8 commit

--- backgr.js
+++ backgr.js
@@ -3,45 +3,69 @@ button1.addEventListener('click', function() {
   obnuleniefon();
   obnulenie2();
   obnulenieopis();
-    document.getElementById("prim01").style.display = "block";
-    document.getElementById("containerop1").style.display = "block";
+  document.getElementById("prim01").style.display = "block";
   this.style.color = 'blue';
+if(vsevse==0) {
+    document.getElementById("containerop1").style.display = "block";
+}
+else{ obnulenieopis();
+}
 });
+
 const button2 = document.getElementById('butn02');
 button2.addEventListener('click', function() {
   obnuleniefon();
   obnulenie2();
   obnulenieopis();
   document.getElementById("prim02").style.display = "block";
-  document.getElementById("containerop1").style.display = "block";
   this.style.color = 'blue';
+if(vsevse==0) {
+    document.getElementById("containerop1").style.display = "block";
+}
+else{ obnulenieopis();
+}
 });
+
 const button3 = document.getElementById('butn03');
 button3.addEventListener('click', function() {
   obnuleniefon();
   obnulenie2();
   obnulenieopis();
   document.getElementById("prim03").style.display = "block";
-  document.getElementById("containerop1").style.display = "block";
   this.style.color = 'blue';
+if(vsevse==0) {
+    document.getElementById("containerop1").style.display = "block";
+}
+else{ obnulenieopis();
+}
 });
+
 const button4 = document.getElementById('butn04');
 button4.addEventListener('click', function() {
   obnuleniefon();
   obnulenie2();
   obnulenieopis();
   document.getElementById("prim04").style.display = "block";
-  document.getElementById("containerop1").style.display = "block";
   this.style.color = 'blue';
+if(vsevse==0) {
+    document.getElementById("containerop1").style.display = "block";
+}
+else{ obnulenieopis();
+}
 });
+
 const button5 = document.getElementById('butn05');
 button5.addEventListener('click', function() {
   obnuleniefon();
   obnulenie2();
   obnulenieopis();
   document.getElementById("prim05").style.display = "block";
-  document.getElementById("containerop2").style.display = "block";
   this.style.color = 'blue';
+if(vsevse==0) {
+    document.getElementById("containerop2").style.display = "block";
+}
+else{ obnulenieopis();
+}
 });
 const button6 = document.getElementById('butn06');
 button6.addEventListener('click', function() {
@@ -49,8 +73,12 @@ button6.addEventListener('click', function() {
   obnulenie2();
   obnulenieopis();
   document.getElementById("prim06").style.display = "block";
-  document.getElementById("containerop2").style.display = "block";
   this.style.color = 'blue';
+if(vsevse==0) {
+    document.getElementById("containerop2").style.display = "block";
+}
+else{ obnulenieopis();
+}
 });
 const button7 = document.getElementById('butn07');
 button7.addEventListener('click', function() {
@@ -58,8 +86,12 @@ button7.addEventListener('click', function() {
   obnulenie2();
   obnulenieopis();
   document.getElementById("prim07").style.display = "block";
-  document.getElementById("containerop3").style.display = "block";
   this.style.color = 'blue';
+if(vsevse==0) { 
+    document.getElementById("containerop3").style.display = "block";
+}
+else{ obnulenieopis();
+}
 });
 const button8 = document.getElementById('butn08');
 button8.addEventListener('click', function() {
@@ -67,8 +99,12 @@ button8.addEventListener('click', function() {
   obnulenie2();
   obnulenieopis();
   document.getElementById("prim08").style.display = "block";
-  document.getElementById("containerop3").style.display = "block";
   this.style.color = 'blue';
+if(vsevse==0) {
+    document.getElementById("containerop3").style.display = "block";
+}
+else{ obnulenieopis();
+}
 });
 const button9 = document.getElementById('butn09');
 button9.addEventListener('click', function() {
@@ -76,8 +112,12 @@ button9.addEventListener('click', function() {
   obnulenie2();
   obnulenieopis();
   document.getElementById("prim09").style.display = "block";
-  document.getElementById("containerop5").style.display = "block";
   this.style.color = 'blue';
+if(vsevse==0) {
+    document.getElementById("containerop3").style.display = "block";
+}
+else{ obnulenieopis();
+}
 });
 const button10 = document.getElementById('butn10');
 button10.addEventListener('click', function() {
@@ -85,8 +125,12 @@ button10.addEventListener('click', function() {
   obnulenie2();
   obnulenieopis();
   document.getElementById("prim10").style.display = "block";
-  document.getElementById("containerop3").style.display = "block";
   this.style.color = 'blue';
+if(vsevse==0) {
+    document.getElementById("containerop4").style.display = "block";
+}
+else{ obnulenieopis();
+}
 });
 const button11 = document.getElementById('butn11');
 button11.addEventListener('click', function() {
@@ -94,8 +138,12 @@ button11.addEventListener('click', function() {
   obnulenie2();
   obnulenieopis();
   document.getElementById("prim11").style.display = "block";
-  document.getElementById("containerop4").style.display = "block";
   this.style.color = 'blue';
+if(vsevse==0) {
+    document.getElementById("containerop4").style.display = "block";
+}
+else{ obnulenieopis();
+}
 });
 const button12 = document.getElementById('butn12');
 button12.addEventListener('click', function() {
@@ -103,8 +151,12 @@ button12.addEventListener('click', function() {
   obnulenie2();
   obnulenieopis();
   document.getElementById("prim12").style.display = "block";
-  document.getElementById("containerop4").style.display = "block";
   this.style.color = 'blue';
+if(vsevse==0) {
+    document.getElementById("containerop5").style.display = "block";
+}
+else{ obnulenieopis();
+}
 });
 const button13 = document.getElementById('butn13');
 button13.addEventListener('click', function() {
@@ -112,8 +164,12 @@ button13.addEventListener('click', function() {
   obnulenie2();
   obnulenieopis();
   document.getElementById("prim13").style.display = "block";
-  document.getElementById("containerop5").style.display = "block";
   this.style.color = 'blue';
+if(vsevse==0) {
+    document.getElementById("containerop5").style.display = "block";
+}
+else{ obnulenieopis();
+}
 });
 const button14 = document.getElementById('butn14');
 button14.addEventListener('click', function() {
@@ -121,8 +177,12 @@ button14.addEventListener('click', function() {
   obnulenie2();
   obnulenieopis();
   document.getElementById("prim14").style.display = "block";
-  document.getElementById("containerop5").style.display = "block";
   this.style.color = 'blue';
+if(vsevse==0) {
+    document.getElementById("containerop5").style.display = "block";
+}
+else{ obnulenieopis();
+}
 });
 
 var obnuleniefon = function(){
@@ -165,3 +225,32 @@ var obnulenieopis = function(){
   document.getElementById("containerop4").style.display = "none";  
   document.getElementById("containerop5").style.display = "none";  
 }
+
+function show_hide6()
+{
+  document.getElementById("containerop1").style.display="none";
+  document.getElementById("containerop2").style.display="none";
+  document.getElementById("containerop3").style.display="none";
+  document.getElementById("containerop4").style.display="none";
+  document.getElementById("containerop5").style.display="none";
+}
+var vsevse=0;
+const ubrfonvse = document.getElementById('ubratfonvse');
+ubrfonvse.addEventListener('click', function() {
+  if(vsevse==1) {  this.style.color = 'red';
+      return vsevse=0;
+  }
+  else{   this.style.color = 'blue';
+      return vsevse=1;
+  }
+});
+
+  // function show_hide6()
+  // {
+  // if(vsevse==1) {
+  //     return vsevse=0;
+  // }
+  // else{ 
+  //     return vsevse=1;
+  // }
+  // }
--- vklvikl2.js
+++ vklvikl2.js
@@ -1,13 +1,43 @@
-var b;
+var с;
 function show_hide2()
 {
-if(b==1) {
+if(с==1) {
     document.getElementById("container").style.display="inline";
-    return b=0;
+    return с=0;
 }
 else{
     document.getElementById("container").style.display="none";
+    return с=1;
+}
+}
+
+var b;
+function show_hide3()
+{
+if(b==1) {
+    document.getElementById("butnfon").style.display="inline";
+    document.getElementById("butnfon1").style.display="inline";
+    return b=0;
+}
+else{
+    document.getElementById("butnfon").style.display="none";
+    document.getElementById("butnfon1").style.display="none";
     return b=1;
 }
 }
+
+var vse;
+function show_hide5()
+{
+if(vse==1) {
+    document.getElementById("victor1").style.display="inline";
+    return vse=0;
+}
+else{
+    document.getElementById("victor1").style.display="none";
+    return vse=1;
+}
+}
+
+
 
